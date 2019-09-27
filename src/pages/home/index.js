5329b000b3efc25ed17bc78a61fbed30640ab492
Michel Lima <michellimadesigner@outlook.com>
2019-09-27 00:29:54 -0300

    Created load API function and created moveis classes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import "./home.css";
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            movies: []
+        }
+        this.loadMovies = this.loadMovies.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadMovies();
+    }
+
+    loadMovies(){
+        // API url
+        let url = 'https://sujeitoprogramador.com/r-api/?api=filmes/';
+
+        fetch(url)
+        .then( response => response.json())
+        .then( json => {
+            this.setState({ movies: json });
+        })
+    }
+
     render(){
         return(
             <div className="app">
-                <h1>Home Page</h1>
+                <div className="movies">
+                    <div className="movies__list">
+                        { this.state.movies.map( (e) => {
+                            return(
+                                <article className="movies__item" key={e.id}>
+                                    <strong className="movies__title"> { e.nome } </strong>
+                                    <div className="movies__shadow"></div>
+                                    <img src={e.foto} alt={e.nome} />
+                                </article>
+                            )
+                        }) }
+                    </div>
+                </div>
             </div>
         )
     }
